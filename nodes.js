899e1fd710e313b0ced21ee647faa43e4b2b4677
Jarett Stevens <jarett.stevens@gmail.com>
2025-09-13 21:19:55 -0500

    reorganize static node data

--- nodes.js
+++ nodes.js
@@ -1,3 +1,203 @@
+/* basic node state */
+var basicNodeState = {
+    kind: "basic",
+    program: [],
+    program_text: [],
+    label_map: {}, /* maps label names to their corresponding instruction indices */
+    program_counter: 0,
+    output: {
+        up: null,
+        down: null,
+        left: null,
+        right: null
+    },
+    acc: 0,
+    bak: 0,
+    last: "N/A",
+    mode: "IDLE",
+    idle: 100,
+    blocked: false,
+    neighbors: {
+        up: null,
+        down: null,
+        left: null,
+        right: null
+    }
+}
+
+/* stack memory node state */
+var stackMemoryNodeState = {
+    kind: "stackmem",
+    stack: [],
+    read: false,
+    output: {
+        up: null,
+        down: null,
+        left: null,
+        right: null
+    },
+    neighbors: {
+        up: null,
+        down: null,
+        left: null,
+        right: null
+    }
+}
+
+/* damaged node state; just null outputs */
+var damagedNodeState = {
+    kind: "damaged",
+    output: {
+        up: null,
+        down: null,
+        left: null,
+        right: null
+    },
+}
+
+/* input values and output state */
+var inputState = {
+    kind: "input-port",
+    label: "",
+    values: [],
+    index: 0,
+    blocked: false,
+    output: {
+        down: null
+    }
+}
+
+/* output values state */
+var outputState =  {
+    kind: "output-port",
+    label: "",
+    values: [],
+    output: {
+        up: null,
+    },
+}
+
+/* current state of all inputs, outputs, and nodes */
+current_state = {
+    nodes: [],
+    input: [],
+    output: []
+}
+
+/* next state of all inputs, outputs, and nodes - will become current state
+for the next clock cycle */
+next_state = {
+    nodes: [],
+    input: [],
+    output: []
+}
+
+/* mapping of all nodes to their neighbors */
+neighbors = {
+    nodes: [
+        /* row 1, node 1 */
+        {
+            up: { l: "input", i: 0},
+            down: { l: "nodes", i: 4},
+            left: null,
+            right: { l: "nodes", i: 1}
+        },
+        /* row 1, node 2 */
+        {
+            up: { l: "input", i: 1},
+            down: { l: "nodes", i: 5},
+            left: { l: "nodes", i: 0},
+            right: { l: "nodes", i: 2}
+        },
+        /* row 1, node 3 */
+        {
+            up: { l: "input", i: 2},
+            down: { l: "nodes", i: 6},
+            left: { l: "nodes", i: 1},
+            right: { l: "nodes", i: 3}
+        },
+        /* row 1, node 4 */
+        {
+            up: { l: "input", i: 3},
+            down: { l: "nodes", i: 7},
+            left: { l: "nodes", i: 2},
+            right: null
+        },
+
+        /* row 2, node 4 */
+        {
+            up: { l: "nodes", i: 0},
+            down: { l: "nodes", i: 8},
+            left: null,
+            right: { l: "nodes", i: 5}
+        },
+        /* row 2, node 5 */
+        {
+            up: { l: "nodes", i: 1},
+            down: { l: "nodes", i: 9},
+            left: { l: "nodes", i: 4},
+            right: { l: "nodes", i: 6}
+        },
+        /* row 2, node 6 */
+        {
+            up: { l: "nodes", i: 2},
+            down: { l: "nodes", i: 10},
+            left: { l: "nodes", i: 5},
+            right: { l: "nodes", i: 7}
+        },
+        /* row 2, node 7 */
+        {
+            up: { l: "nodes", i: 3},
+            down: { l: "nodes", i: 11},
+            left: { l: "nodes", i: 6},
+            right: null
+        },
+
+        /* row 3, node 8 */
+        {
+            up: { l: "nodes", i: 4},
+            down: { l: "output", i: 0},
+            left: null,
+            right: { l: "nodes", i: 9}
+        },
+        /* row 3, node 9 */
+        {
+            up: { l: "nodes", i: 5},
+            down: { l: "output", i: 1},
+            left: { l: "nodes", i: 8},
+            right: { l: "nodes", i: 10}
+        },
+        /* row 3, node 10 */
+        {
+            up: { l: "nodes", i: 6},
+            down: { l: "output", i: 2},
+            left: { l: "nodes", i: 9},
+            right: { l: "nodes", i: 11}
+        },
+        /* row 3, node 11 */
+        {
+            up: { l: "nodes", i: 7},
+            down: { l: "output", i: 3},
+            left: { l: "nodes", i: 10},
+            right: null
+        }
+    ],
+    /* neighbors configuration for each input in the puzzle */
+    inputs: [
+        { down: { l: "nodes", i: 0}},
+        { down: { l: "nodes", i: 1}},
+        { down: { l: "nodes", i: 2}},
+        { down: { l: "nodes", i: 3}},
+    ],
+    /* neighbors configuration for each output in the puzzle */
+    outputs: [
+       { up: { l: "nodes", i: 8}},
+       { up: { l: "nodes", i: 9}},
+       { up: { l: "nodes", i: 10}},
+       { up: { l: "nodes", i: 11}},
+    ]
+}
+
 var basicNodeHtml = '\
   <div class="node basic-node" id="node-${id}">\
     <div class="node-arrows">\
